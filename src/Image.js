43482c7b38c18c7694457b5956b1cc95b1ba044c
tatianasnook <tatsnook@gmail.com>
2023-10-21 13:40:46 -0500

    add extra image component

---
+++ src/Image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import image from './comp2.jpg';
+
+function Image() {
+  return (
+    <div>
+      <img src={image} alt="" width="300px"/>
+    </div>
+  )
+}
+
+export default Image;
