1cc16af70e7cecb0e62d4be559ff1c06725220f7
tatianasnook <tatsnook@gmail.com>
2023-10-21 13:40:46 -0500

    add extra image component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Comp from './Developers';
+import Image from './Image';
 
 function App() {
   return (
     <div className="App">
       <h1>Studio23</h1>
       <Comp />
+      <Image />
     </div>
   );
 }
