ba1df53780e833ea6a8ad7923e5e1d4141f0de0c
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2023-10-21 20:18:53 +0300

    I added img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Comp from './Developers';
 
 function App() {
   return (
     <div className="App">
       <h1>Studio23</h1>
+      <Comp />
     </div>
   );
 }
---
+++ src/Developers.js
@@ -0,0 +1,11 @@
+import Dev from './comp.jpg'
+
+function Comp (){
+    return(
+        <div>
+            <img src={Dev} alt='me' width="300px" />
+        </div>
+    )
+}
+
+export default Comp;
