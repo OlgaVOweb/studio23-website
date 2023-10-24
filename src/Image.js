8bf73f5c0ecc576e8ab40a1b06587ee4c09f497b
tatianasnook <tatsnook@gmail.com>
2023-10-24 14:29:02 -0500

    new image added

--- src/Image.js
+++ src/Image.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import image from './comp2.jpg';
+import newImage from './new-image.jpg';
 
 function Image() {
   return (
     <div>
-      <img src={image} alt="" width="300px"/>
+      <img src={image} alt="computer" width="300px"/>
+      <img src={newImage} alt="computer" width="400px"/>
     </div>
   )
 }
